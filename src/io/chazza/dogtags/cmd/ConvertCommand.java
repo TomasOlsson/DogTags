1b73fbbc244dfffd630d225e360f795f3f8709ef
TomasOlsson <tommen89@hotmail.com>
2017-11-09 14:10:36 +0100

    Fix for MYSQL and FLATFILE

--- src/io/chazza/dogtags/cmd/ConvertCommand.java
+++ src/io/chazza/dogtags/cmd/ConvertCommand.java
@@ -5,6 +5,7 @@ import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.Subcommand;
 import io.chazza.dogtags.DogTags;
+import io.chazza.dogtags.StorageEnum;
 import io.chazza.dogtags.StorageHandler;
 import io.chazza.dogtags.TagLang;
 import io.chazza.dogtags.dev.DogTag;
@@ -17,6 +18,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,6 +29,7 @@ import java.io.IOException;
 @CommandAlias("dtconvert")
 public class ConvertCommand extends BaseCommand implements Listener {
 
+	
     @Subcommand("DeluxeTags") @CommandCompletion("deluxetags")
     public void onCommand(CommandSender sender){
         if(!sender.hasPermission("dogtags.convert")) {sender.sendMessage(TagLang.NO_PERMISSION.get()); return; }
@@ -35,20 +38,22 @@ public class ConvertCommand extends BaseCommand implements Listener {
         if(!f.exists()) return;
 
         FileConfiguration fc = YamlConfiguration.loadConfiguration(f);
-        FileConfiguration config = YamlConfiguration.loadConfiguration(new File(DogTags.getInstance().getDataFolder(), "config.yml"));
 
+        
         for(String tags : fc.getConfigurationSection("deluxetags").getKeys(false)){
             String prefix = fc.getString("deluxetags."+tags+".tag");
             String description = fc.getString("deluxetags."+tags+".description");
-
-            config.set("dogtags."+tags+".prefix", prefix);
-            config.set("dogtags."+tags+".description", description);
-            config.set("dogtags."+tags+".permission", true);
-            LogUtil.outputMsg("Converted "+tags+" with prefix "+prefix + " and description "+description);
+            
+            	StorageHandler.addTag(tags, prefix, description, true);
+            	
+            	DogTag dogTag = new DogTag(tags);
+            	dogTag.withDescription(description).withPrefix(prefix).withPermission(true).build(DogTags.getInstance());
+            	LogUtil.outputMsg("Converted "+tags+" with prefix "+prefix + " and description "+description);
+            	
         }
 
-           // config.save(new File(DogTags.getInstance().getDataFolder(), "config.yml"));
-            DogTags.getInstance().handleReload();
+           //config.save(new File(DogTags.getInstance().getDataFolder(), "config.yml"));
+        	//DogTags.getInstance().handleReload();
         if(sender instanceof Player) sender.sendMessage("§6[§eDogTags§6] §fCheck Console for Information.");
     }
 }
